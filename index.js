769488d852a7ebf7c2e89d520d03d341872097ee
WellingtonVinii <wellingtonvinii10@gmail.com>
2024-11-08 00:00:27 -0300

    concluir o projeto

---
+++ index.js
@@ -0,0 +1,170 @@
+import express from 'express';
+
+const app = express();
+//configurar a aplicação para receber os dados do formulário
+app.use(express.urlencoded({ extended: true }));
+const porta = 3000;
+const host = 'localhost';//ip de todas as interfaces (placas de rede do pc)
+
+var listaClientes = [];
+
+function cadastrarClienteView(req, res) {
+    res.send(`
+        <!DOCTYPE html>
+        <html lang="pt-br">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Cadastro de Cliente</title>
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        </head>
+        <body>
+            <div class="container mt-5">
+                <h1>Informações do Cliente</h1>
+                <h2>Preencha os campos abaixo</h2>
+                <form action="/cadastrarCliente" method="POST">
+                    <div class="mb-3">
+                        <label for="nome" class="form-label">Nome Completo</label>
+                        <input type="text" class="form-control" id="nome" name="nome" placeholder="Informe seu nome completo">
+                    </div>
+                    <div class="mb-3">
+                        <label for="cpf" class="form-label">CPF</label>
+                        <input type="text" class="form-control" id="cpf" name="cpf" placeholder="000.000.000-00">
+                    </div>
+                    <div class="mb-3">
+                        <label for="nascimento" class="form-label">Data de Nascimento</label>
+                        <input type="date" class="form-control" id="nascimento" name="nascimento">
+                    </div>
+                    <div class="mb-3">
+                        <label for="email" class="form-label">E-mail</label>
+                        <input type="email" class="form-control" id="email" name="email" placeholder="exemplo@dominio.com">
+                    </div>
+                    <div class="mb-3">
+                        <label for="telefone" class="form-label">Telefone</label>
+                        <input type="text" class="form-control" id="telefone" name="telefone" placeholder="(99) 99999-9999">
+                    </div>
+                    <div class="mb-3">
+                        <label for="genero" class="form-label">Gênero</label>
+                        <select class="form-select" id="genero" name="genero">
+                            <option>Masculino</option>
+                            <option>Feminino</option>
+                            <option>Outro</option>
+                        </select>
+                    </div>
+                    <div class="mb-3">
+                        <label for="observacoes" class="form-label">Observações</label>
+                        <textarea class="form-control" id="observacoes" name="observacoes" rows="4" placeholder="Insira qualquer observação adicional aqui"></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Enviar</button>
+                </form>
+            </div>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+        </body>
+        </html>
+    `);
+}
+
+function menuCliente(req,resp){
+    resp.send(`
+        <!DOCTYPE html>
+        <html lang="pt-br">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Cadastro de Cliente</title>
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        </head>
+        <body>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Menu Principal</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Alternar navegação">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/cadastrarCliente">Cadastro de Clientes</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+</body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</html> `
+    )
+}
+
+function cadastrarCliente(req,resp){
+    const nome = req.body.nome;
+    const cpf = req.body.cpf;
+    const nascimento = req.body.nascimento;
+    const email = req.body.email;
+    const telefone = req.body.telefone;
+    const genero = req.body.genero;
+    const observacoes = req.body.observacoes;
+
+    const cliente = { nome, cpf, nascimento, email, telefone, genero, observacoes };
+    listaClientes.push(cliente);
+
+    listaClientes.push();
+   
+    resp.write(`   
+        <!DOCTYPE html>
+        <html lang="pt-br">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Lista de Cliente</title>
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        </head>
+        <body>
+        <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Nome</th>
+      <th scope="col">CPF</th>
+      <th scope="col">Nascimento</th>
+      <th scope="col">E-mail</th>
+      <th scope="col">Telefone</th>
+      <th scope="col">Genero</th>
+      <th scope="col">Observações</th>
+    </tr>
+  </thead>
+  <tbody>`);
+
+  for (var i = 0; i < listaClientes.length; i++){
+    resp.write(`<tr>
+                <td>${listaClientes[i].nome}</td>
+                <td>${listaClientes[i].cpf}</td>
+                <td>${listaClientes[i].nascimento}</td>
+                <td>${listaClientes[i].email}</td>
+                <td>${listaClientes[i].telefone}</td>
+                <td>${listaClientes[i].genero}</td>
+                <td>${listaClientes[i].observacoes}</td>
+                </tr>
+        `)
+  }
+
+  resp.write(`
+        </tbody>
+        </table>
+        <a class="btn btn-dark" href="/cadastrarCliente" role="button">Continuar cadastrando</a>
+        <a class="btn btn-dark" href="/" role="button">Voltar para o menu</a>
+        </body>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+        </html>
+  `);
+
+resp.end();
+
+}
+
+app.get('/',menuCliente);
+app.get('/cadastrarCliente', cadastrarClienteView);//enviar o formulario
+app.post('/cadastrarCliente',cadastrarCliente)
+
+app.listen(porta, host, () => {
+    console.log('Servidor iniciado e em execução no endereço http://localhost:3000');
+})
